fa57fd5538e1f0c37419849d1f804a4c223403c0
Horváth Bettina <bettina.horvath@visuallabs.hu>
2022-05-23 15:17:59 +0200

    sagemcom szerviz ossz nezet megnyitasa

---
+++ Microsoft.Dynamics365.UIAutomation.Sample/VLTestCases/VLUCI/10819TestCase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System;
+using System.Security;
+namespace Microsoft.Dynamics365.UIAutomation.Sample.VLTestCases.VLUCI
+{
+    [TestClass]
+    public class _10819TestCase
+    {
+      
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly SecureString _mfaSecretKey = System.Configuration.ConfigurationManager.AppSettings["MfaSecretKey"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void UCITestShowAllCasesSagemcom()
+        {
+            var client = new WebClient(TestSettings.Options);
+            using (var xrmApp = new XrmApp(client))
+            {
+                xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+
+                xrmApp.Navigation.OpenApp(UCIAppName.FieldService);
+
+                xrmApp.Navigation.OpenSubArea(UCIAppName.FieldService, "Cases");
+
+                // new contact
+
+                xrmApp.Grid.SwitchView("Sagemcom szerviz esetek - Összes eset");
+
+            }
+
+        }
+    }
+}
